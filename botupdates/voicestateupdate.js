165b0e4ba94c92c741dab9ee3b61ce809bbe0788
Elias <elias.knodel@gmail.com>
2019-09-24 19:00:10 +0200

    fixed: no auto channel on mute parent join

--- botupdates/voicestateupdate.js
+++ botupdates/voicestateupdate.js
@@ -1,60 +1,61 @@
+/* Modules */
 const Discord = require("discord.js");
 const fs = require("fs");
-// require
-const botconfig = require("../data/settings/botconfig.json");
-const userData = require("../data/rpg/userdata.json");
 
-// auto channel
+/* Data */
+
+// require
 const usernameautochannelfile = require(`./autochannel/withusername.js`);
 const gameautochannelfile = require(`./autochannel/withgame.js`);
 
 module.exports.run = async (bot, oldMember, newMember) => {
 
-let newUserChannel = newMember.voiceChannel
-let oldUserChannel = oldMember.voiceChannel
+    let newUserChannel = newMember.voiceChannel
+    let oldUserChannel = oldMember.voiceChannel
 
-if(oldMember.presence.game === null) {
-    var gamename = "No Game"
-} else {
-    var gamename = oldMember.presence.game.name
-}
+    if (oldMember.presence.game == null) {
+        var gamename = "No Game"
+    } else {
+        var gamename = oldMember.presence.game.name
+    }
 
-// let channel = bot.channels.find(x => x.name.startsWith(`🎮`));
+    // If Member mutes himself | unmuted before & muted after
+    // if (oldMember.mute == false && newMember.mute == true) return;
+    // if (oldMember.mute == true && newMember.mute == false) return;
 
-// If Member mutes himself | unmuted before & muted after
-    if(oldMember.mute === false && newMember.mute === true) return;
-    if(oldMember.mute === true && newMember.mute === false) return; 
-// If Member joins VC | From undefined in defined Channel
-    if(oldUserChannel === undefined && newUserChannel !== undefined) {    
+    // If Member joins VC | From undefined in defined Channel
+    if (oldMember.mute == false && newMember.mute == true && oldUserChannel == undefined && newUserChannel != undefined || oldMember.mute == true && newMember.mute == false && oldUserChannel == undefined && newUserChannel != undefined || oldUserChannel == undefined && newUserChannel != undefined) {
+
+        console.log(oldMember.mute, newMember.mute)
 
         let executable = "yes";
         if (newUserChannel.name.startsWith(`🎮`)) gameautochannelfile.run(bot, oldMember, newMember, executable);
         if (newUserChannel.name.startsWith(`👥`)) usernameautochannelfile.run(bot, oldMember, newMember);
 
-// If Member switches VC | From defined in defined Channel 
-    } else if(oldUserChannel !== undefined && newUserChannel !== undefined) {     
+        // If Member switches VC | From defined in defined Channel 
+    } else if (oldUserChannel != undefined && newUserChannel != undefined) {
 
-        if(oldUserChannel.name.startsWith("- ")) {
-            if(oldUserChannel.name.startsWith("- " + gamename)) {
-                let executable = "no"; 
-                gameautochannelfile.run(bot, oldMember, newMember, executable);   
+        if (oldUserChannel.name.startsWith("- ")) {
+            if (oldUserChannel.name.startsWith("- " + gamename)) {
+                let executable = "no";
+                gameautochannelfile.run(bot, oldMember, newMember, executable);
             }
-            if(oldUserChannel.members.size === 0) oldUserChannel.delete();
+            if (oldUserChannel.members.size == 0) oldUserChannel.delete();
         }
 
         let executable = "yes";
         if (newUserChannel.name.startsWith(`🎮`)) gameautochannelfile.run(bot, oldMember, newMember, executable);
         if (newUserChannel.name.startsWith(`👥`)) usernameautochannelfile.run(bot, oldMember, newMember);
 
-// If Member leaves VC | From defined in undefined Channel
-    } else if(oldUserChannel !== undefined && newUserChannel === undefined) {  
+        // If Member leaves VC | From defined in undefined Channel
+    } else if (oldUserChannel != undefined && newUserChannel == undefined) {
 
-        if(oldUserChannel.name.startsWith("- ")) {
-            if(oldUserChannel.name.startsWith("- " + gamename)) {
-                let executable = "no"; 
-                gameautochannelfile.run(bot, oldMember, newMember, executable);  
+        if (oldUserChannel.name.startsWith("- ")) {
+            if (oldUserChannel.name.startsWith("- " + gamename)) {
+                let executable = "no";
+                gameautochannelfile.run(bot, oldMember, newMember, executable);
             }
-            if(oldUserChannel.members.size === 0) oldUserChannel.delete();
+            if (oldUserChannel.members.size == 0) oldUserChannel.delete();
         }
     }
 
